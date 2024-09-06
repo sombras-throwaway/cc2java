2becb8abd8a395850faac184f1b88ad615b37bf2
sombras-throwaway <62824834+sombras-throwaway@users.noreply.github.com>
2024-09-06 08:44:24 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc2.pkg1b.opolinto;
+
+/**
+ *
+ * @author m303user
+ */
+
+/*Anna
+September 6, 2024
+First Programming Session*/
+public class Test {
+    public static void main(String[] args){
+        System.out.print("Anna \t\t Rhodora M. \nQuitaleg");
+        //Java Escape Characters
+        System.out.print("\n\n\nAnna\"s Class");
+        System.out.println("I love Java programming");
+    }
+}
